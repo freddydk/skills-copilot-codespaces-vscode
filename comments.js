e2cc9dc188118c1b1d8621b64a9708fafefd437e
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2024-06-10 13:31:18 +0000

    add comments

---
+++ comments.js
@@ -0,0 +1,87 @@
+// Create web server and listen to port 3000
+const express = require('express');
+const app = express();
+const comments = require('./comments.json');
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+
+// Path: index.js
+// Create web server and listen to port 3000
+const express = require('express');
+const app = express();
+const comments = require('./comments.json');
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+
+// Path: comments.js
+// Create web server and listen to port 3000
+const express = require('express');
+const app = express();
+const comments = require('./comments.json');
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+
+// Path: index.js
+// Create web server and listen to port 3000
+const express = require('express');
+const app = express();
+const comments = require('./comments.json');
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+
+// Path: comments.js
+// Create web server and listen to port 3000
+const express = require('express');
+const app = express();
+const comments = require('./comments.json');
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+
+// Path: index.js
+// Create web server and listen to port 3000
+const express = require('express');
+const app = express();
+const comments = require('./comments.json');
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+
+// Path: comments.js
+// Create web server and listen to port 3000
+const express = require('express');
+const app = express
